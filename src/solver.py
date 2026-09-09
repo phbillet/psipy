15dec5f0b8604b9e33e9adc21faac3f8d3298909
Philippe Billet <philippe.billet@seznam.cz>
2026-09-09 17:10:05 +0200

    new notebook about 2D symbol

--- src/solver.py
+++ src/solver.py
@@ -1734,7 +1734,7 @@ class PDESolver:
                     boundary_condition=self.boundary_condition,
                     dealiasing_mask=self.dealiasing_mask,
                     backend='peetre',
-                    joint_backend='direct',
+                    joint_backend='auto',
                 )
             elif self.dim == 2:
                 contribution = psi_op.apply(
@@ -1746,7 +1746,7 @@ class PDESolver:
                     boundary_condition=self.boundary_condition,
                     dealiasing_mask=self.dealiasing_mask,
                     backend='peetre',
-                    joint_backend='direct',
+                    joint_backend='auto',
                 )
             else:
                 raise ValueError("Only 1D and 2D supported")
